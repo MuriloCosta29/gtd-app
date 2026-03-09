615afee0fb505fa0fd20ac2710e11e411a6b4ceb
Carlos Murilo de O. C. F. <murilojagger@gmail.com>
2026-03-09 19:52:37 -0300

    feat: add inbox endpoints

--- main.py
+++ main.py
@@ -1,9 +1,26 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+inbox = []
 
 app = FastAPI()
 
 
 @app.get("/")
 def health_check():
-    # APIs retornam JSON, e JSON é estruturado como chave e valor.
-    return {"status": "A API está rodando"}
+    return {"status": "A api está funcionando corretamente."}
+
+
+class InboxItem(BaseModel):
+    text: str
+
+
+@app.post("/inbox")
+def cria_inbox_item(item: InboxItem):
+    inbox.append(item.text)
+    return {"item": "inbox adicionado com sucesso.", "texto": item.text}
+
+
+@app.get("/inbox")
+def retorna_inbox():
+    return {"inbox": inbox}
