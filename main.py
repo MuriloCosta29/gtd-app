590edd3df5fa680e2dc3ee5128e62712db01dfe4
Carlos Murilo de O. C. F. <murilojagger@gmail.com>
2026-03-10 12:15:23 -0300

    refactor: rename functions to english

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ def health_check():
     return {"status": "A api está funcionando corretamente."}
 
 
-class InboxItem(BaseModel):
+class InboxItem(BaseModel):  # Validates what arrives in the request
     text: str
 
 
 @app.post("/inbox")
-def cria_inbox_item(item: InboxItem):
+def make_inbox_item(item: InboxItem):
     inbox.append(item.text)
     return {"item": "inbox adicionado com sucesso.", "texto": item.text}
 
 
 @app.get("/inbox")
-def retorna_inbox():
+def return_inbox():
     return {"inbox": inbox}
