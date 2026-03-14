0b9761c1850e4185ffc5c9360a90688bc8564a33
Carlos Murilo de O. C. F. <murilojagger@gmail.com>
2026-03-14 15:02:35 -0300

    feat: add delete inbox endpoint

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
-from fastapi import FastAPI, Depends
+# API | Routes | Schemas | Applications instance.
 
-# Depends tell to FastAPI "before running this route, inject this dependency as a parameter."
+# ------------------------------------------------------------------------------------------------
+
+from fastapi import (
+    FastAPI,
+    Depends,
+    HTTPException,
+)  # Depends() -> what have inside parantheses, the FastAPI call and say: "before running this route, call what's inside () and inject the result as db(example.)"
+
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 from database import get_session
 from models import InboxItemModel
 from models import Base
 from database import engine
-# when i call get_db inside de function, FastAPI doens't know it's a dependecy -- it's just a function call that does nothing useful there.
 
-Base.metadata.create_all(bind=engine)
+# ------------------------------------------------------------------------------------------------
+
+Base.metadata.create_all(bind=engine)  # Creates the table in the database file.
 app = FastAPI()
+
+# ------------------------------------------------------------------------------------------------
 
 
 @app.get("/")
 def health_check():
-    return {"status": "A api está funcionando corretamente."}
+    return {
+        "status": "A api está funcionando corretamente."
+    }  # Check if API is working.
+
+
+# ------------------------------------------------------------------------------------------------
 
 
 class InboxItem(BaseModel):  # Validates what arrives in the request
     text: str
 
 
-# ------------------------------------------------------------------------------------------------------------
-# NOTE: I can't add a class!!! I have to create de instance first and after this call the instance
-# db.add() -> adds the object to the session
-# db.commit() -> Saves to the database
-# db.refresh -> updates the object with data from the database
+# ------------------------------------------------------------------------------------------------
+
+
 @app.post("/inbox")
 def make_inbox_item(
     item: InboxItem, db: Session = Depends(get_session)
@@ -38,9 +51,30 @@ def make_inbox_item(
     return {"item": "inbox adicionado com sucesso.", "texto": item.text}
 
 
-# ------------------------------------------------------------------------------------------------------------
-# For GET i don't need commit() and refresh()
+# ------------------------------------------------------------------------------------------------
+
+
+# GET i don't need commit() and refresh()
 @app.get("/inbox")
 def return_inbox(db: Session = Depends(get_session)):
     tasks = db.query(InboxItemModel).all()  # Save result in a variable
     return {"inbox": tasks}  # Return the variable with result.
+
+
+# ------------------------------------------------------------------------------------------------
+
+# .all() -> return the entire list
+# .first() -> Return only the first item of that list -- or `None` if nothing was found. For delete by id i only need one specific task.
+
+
+@app.delete("/inbox/{id}")
+def delete_task(id: int, db: Session = Depends(get_session)):
+    task = db.query(InboxItemModel).filter(InboxItemModel.id == id).first()
+    if task is None:
+        raise HTTPException(status_code=404, detail="Task not found")
+    db.delete(task)
+    db.commit()
+    return {"message": "Task deletada com sucesso", "id": id}
+
+
+# ------------------------------------------------------------------------------------------------
